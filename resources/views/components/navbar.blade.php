f46d118a4d3c08975e10332a370ee9732f3d45e4
suwantara <Suwantaraputra1@gmail.com>
2025-05-19 20:26:01 +0800

    update mobile navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -11,7 +11,7 @@
 
         <label for="menu-toggle" class="pointer-cursor block md:hidden"><svg class="fill-current text-white"
                 xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-                <title>menu</title>
+                <title>Menu</title>
                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
             </svg></label>
 
